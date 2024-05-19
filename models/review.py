c9a3ad3c6fffb1e470e7cc9cc44538a70175300c
mosignorjude <mosignorjude2@gmail.com>
2024-05-19 13:40:26 +0100

    console v1 - documentation

--- models/review.py
+++ models/review.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
 """ Defines the review class.
     Inherits from BaseModel class
 """
+from models.base_model import BaseModel
 
 
 class Review(BaseModel):
--- models/state.py
+++ models/state.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
 """ Defines the State class.
     Inherits from BaseModel class
 """
+from models.base_model import BaseModel
 
 
 class State(BaseModel):
